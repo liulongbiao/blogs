c6709776e7d3ba833424394e3a588f5ee09d4ba5
liulongbiao <liulongbiao@cnstrong.cn>
2018-02-02 09:26:35 +0800

    add gatsby-plugin-styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`
   ]
 };
